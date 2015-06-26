5c2c178872821de98a372549e574405c4eb68277
Whitney May <maywhitney@rocketmail.com>
2015-06-26 12:47:12 -0400

    Bowling Game Kata
    First Commit

---
+++ BowlingGame.Tests/Tests.cs
@@ -0,0 +1,73 @@
+﻿using BowlingGame6215;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Game g;
+        [SetUp]
+        public void Setup()
+        {
+            g = new Game();
+        }
+
+        [Test]
+        public void TestforOneRoll()
+        {
+            g.Roll(3);
+            Assert.AreEqual(3, g.Score());
+        }
+
+        [Test]
+        public void TestforGutterGame()
+        {
+            RollMany(20, 0);
+            Assert.AreEqual(0, g.Score());
+        }
+
+        [Test]
+        public void TestforAllOnes()
+        {
+            RollMany(20, 1);
+            Assert.AreEqual(20, g.Score());
+        }
+
+        [Test]
+        public void TestforSpare()
+        {
+            RollSpare();
+            g.Roll(3);
+            Assert.AreEqual(16, g.Score());
+        }
+
+        [Test]
+        public void TestforStrike()
+        {
+            g.Roll(10);
+            g.Roll(3);
+            g.Roll(4);
+            Assert.AreEqual(24, g.Score());
+        }
+
+        private void RollMany(int r, int pins)
+        {
+            for (var i = 0; i < r; i++)
+            {
+                g.Roll(pins);
+            }
+        }
+
+        private void RollSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
+        }
+    }
+}
---
+++ BowlingGame6215/Game.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame6215
+{
+    public class Game
+    {
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] += pins;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int frame = 0;
+            for (int r = 0; r < 10; r++ )
+            {
+                if (IsStrike(frame))
+                {
+                    score += 10 + StrikeBonus(frame);
+                    frame++;
+                }
+                else if (IsSpare(frame))
+                {
+                    score += 10 + SpareBonus(frame);
+                    frame += 2;
+                }
+                else
+                {
+                    score += rolls[frame] + rolls[frame + 1];
+                    frame += 2;
+                }
+            }
+
+              return score;
+        }
+
+        private bool IsSpare(int frame)
+        {
+            return rolls[frame] + rolls[frame + 1] == 10;
+        }
+
+        private bool IsStrike(int frame)
+        {
+            return rolls[frame] == 10;
+        }
+
+        private int SpareBonus(int frame)
+        {
+            return rolls[frame + 2];
+        }
+
+        private int StrikeBonus(int frame)
+        {
+            return rolls[frame + 1] + rolls[frame + 2];
+        }
+
+
+    }
+}
